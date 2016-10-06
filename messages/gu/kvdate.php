39f324474bafc33f413cc3fbc68bd2bfb69450ca
Vishal Bhalodiya <bhalodiyavishal46@gmail.com>
2016-10-06 12:18:04 +0530

    Update kvdate.php

--- messages/gu/kvdate.php
+++ messages/gu/kvdate.php
@@ -15,6 +15,8 @@
  * of the guide for details.
  *
  * NOTE: this file must be saved in UTF-8 encoding.
+ *
+ * @author Vishal Bhalodiya <bhalodiyavishal46@gmail.com>
  */
 return [
     'Clear field' => 'ફીલ્ડ્ ખાલી કરો',
