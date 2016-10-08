873f74655c67fafa4227910ca6992594c87fab7a
Edo Freriks <edofre@gmail.com>
2016-10-08 14:10:56 +0200

    Added Dutch (NL) translations

--- messages/nl/kvdate.php
+++ messages/nl/kvdate.php
@@ -17,6 +17,6 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Clear field' => '',
-    'Select date' => '',
+    'Clear field' => 'Leeg veld',
+    'Select date' => 'Selecteer datum',
 ];
